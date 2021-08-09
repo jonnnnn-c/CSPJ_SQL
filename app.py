e28ae39623b8333ada87c1e0ed49fea76c6027bf
jonnnnn-c <68585816+jonnnnn-c@users.noreply.github.com>
2021-08-10 00:28:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from website import create_app
 from flask import Flask
 
+with connection.cursor() as cursor:
+    cursor.execute('SELECT COUNT(*) FROM users')
+    result = cursor.fetchone()
+print(result)
+
 app = create_app()
 
 if __name__ == '__main__':
